45e315ea109292c5d3923fdeafa192130a7943f4
JacksonHonoro <jackson.honoro@gmail.com>
2022-09-16 10:42:44 -0300

    chore: pagina inicial - aula 2

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Background } from './src/components/Background'
+
+export default function App() {
+  return (
+    <Background>
+
+    </Background>
+  );
+}
+
+
